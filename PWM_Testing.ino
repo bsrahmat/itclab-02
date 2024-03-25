ce14d5429a4035467a653ef6919b89b51c90b082
Assoc. Prof. Dr. Basuki Rahmat, S.Si, MT, ITS-AI <48795125+bsrahmat@users.noreply.github.com>
2024-03-25 09:22:37 +0700

    Add files via upload

---
+++ PWM_Testing.ino
@@ -0,0 +1,41 @@
+/*********************************************************************************
+ * Program   : PWM_Testing
+ * By        : Assoc. Prof. Dr. Basuki Rahmat, S.Si, MT, ITS-AI,
+ *             Assoc. Prof. Dr. Muljono, S.Si, M.Kom, et al
+ * Pro. Team : i-ot.net, io-t.net
+ * R. Group  : Intelligent Control, Robotics and Automation Systems Research Group
+ * Univ.     : Universitas Pembangunan Nasional "Veteran" Jawa Timur
+ * Country   : Indonesia
+ *********************************************************************************/
+ 
+// the number of the LED pin
+const int ledPin = 26;
+
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
+ 
+void setup(){
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
+}
+ 
+void loop(){
+  // increase the LED brightness
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);
+    delay(20);
+  }
+
+  // decrease the LED brightness
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
+    delay(20);
+  }
+}
